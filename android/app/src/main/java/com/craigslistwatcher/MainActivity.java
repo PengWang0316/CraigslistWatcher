4242969bac653d5191ee0d65181edc5148c0c854
Kevin W <rocwang@cityuniversity.edu>
2018-04-07 15:59:53 -0700

    Added Anodroid and Ios folders.

---
+++ android/app/src/main/java/com/craigslistwatcher/MainActivity.java
@@ -0,0 +1,15 @@
+package com.craigslistwatcher;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "CraigslistWatcher";
+    }
+}
