dd2375746003f2a921e173b871b1c317f1f7df9b
Kevin W <rocwang@cityuniversity.edu>
2018-04-09 16:45:59 -0700

    Added fetchLists function to ListActions as well as the test code.

--- App/actions/ListActions.js
+++ App/actions/ListActions.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-
+import * as CraigslistSearch from 'craigslist-searcher';
 import { FETCH_LISTS_SUCCESS } from './ActionTypes';
 import { AMOUNT_ITEMS_IN_ONE_PAGE } from '../config';
 
@@ -15,25 +14,7 @@ const fetchListsSuccess = lists => ({
   lists
 });
 
-// export const fetchLists = page => dispatch => getCraigslistClient().list({ offset: page * AMOUNT_ITEMS_IN_ONE_PAGE }).then(lists => dispatch(fetchListsSuccess(lists)));
-
-export const fetchLists = page => dispatch => {
-  const regexFindResult = /<p class="result-info">.+?datetime="(.+?)".+?href="(.+?)".+?>(.+?)<\/a>.+?price">(.+?)<.+?hood">(.+?)<.+?<\/p>/g;
-  return axios.get('https://seattle.craigslist.org/search/sss?query=surface&sort=rel').then(({ data }) => {
-    const formatedData = data.replace(/\n/g, '');
-    const dataResult = [];
-    let matchArray;
-    while (matchArray = regexFindResult.exec(formatedData)) {
-      dataResult.push({
-        datetime: matchArray[1],
-        url: matchArray[2],
-        title: matchArray[3],
-        price: matchArray[4],
-        region: matchArray[5]
-      });
-    }
-    dispatch(fetchListsSuccess(dataResult));
-  });
-};
+export const fetchLists = (page = 1) => dispatch =>
+  CraigslistSearch.search({ city: 'seattle', query: 'surface book', offset: (page - 1) * AMOUNT_ITEMS_IN_ONE_PAGE }).then(data => dispatch(fetchListsSuccess(data)));
 
 export default fetchLists;
--- __tests__/actions/ListActions.js
+++ __tests__/actions/ListActions.js
@@ -1,6 +1,5 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import axios from 'axios';
 
 import { FETCH_LISTS_SUCCESS } from '../../App/actions/ActionTypes';
 import { fetchLists } from '../../App/actions/ListActions';
@@ -8,23 +7,16 @@ import { fetchLists } from '../../App/actions/ListActions';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-// jest.mock('node-craigslist', () => ({ Client: {} }));
+jest.mock('craigslist-searcher', () => ({ search: jest.fn().mockReturnValue(new Promise(resolve => resolve([{ id: 'id' }]))) }));
 
 describe('ListActions Test', () => {
-  // test('fetchLists', () => {
-  //   const lists = [{ id: 'id' }];
-  //   const expectedActions = [{ type: FETCH_LISTS_SUCCESS, lists }];
-  //   const craigslist = require('node-craigslist');
-  //   const listMockFn = jest.fn().mockReturnValue(new Promise(resolve => resolve(lists)));
-  //   craigslist.Client = function () {};
-  //   craigslist.Client.prototype.list = listMockFn;
-  //   // jest.fn().mockReturnValue({ list: listMockFn });
-  //   const store = mockStore();
-  //   return store.dispatch(fetchLists()).then(() =>
-  //     expect(store.getActions()).toEqual(expectedActions));
-  // });
-
   test('fetchLists', () => {
-
+    const expectedActions = [{ type: FETCH_LISTS_SUCCESS, lists: [{ id: 'id' }] }];
+    // const CraigslistSearch = require('craigslist-searcher');
+    // CraigslistSearch.search = jest.fn().mockReturnValue(new Promise(resolve => resolve(lists)));
+    const store = mockStore();
+    return store.dispatch(fetchLists()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
   });
 });
