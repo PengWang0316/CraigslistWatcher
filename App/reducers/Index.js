dd45db4feff711b111baa5e6891fc0a14090b0e2
Kevin W <rocwang@cityuniversity.edu>
2018-04-14 16:33:09 -0700

    Integrated craislistStates in the root reducer.

--- App/reducers/Index.js
+++ App/reducers/Index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
 
 import lists from './Lists';
-import city from './City';
-import category from './Category';
-import keyword from './Keyword';
+import craigslistStates from './CraigslistStates';
 
 export default combineReducers({
-  category,
-  city,
-  keyword,
+  craigslistStates,
   lists
 });
