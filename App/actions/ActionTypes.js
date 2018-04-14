87fd10eaf1086b25149eb43429852e8a79629eb5
Kevin W <rocwang@cityuniversity.edu>
2018-04-14 16:27:52 -0700

    Added new action types for CraigslistStatesActions.

--- App/actions/ActionTypes.js
+++ App/actions/ActionTypes.js
@@ -3,3 +3,4 @@ export const FETCH_MORE_LISTS_SUCCESS = 'fetch_more_lists_success';
 export const CHANGE_CITY_SUCCESS = 'change_city_success';
 export const CHANGE_CATEGORY_SUCCESS = 'change_category_success';
 export const CHANGE_KEYWORD_SUCCESS = 'change_keword_success';
+export const INCREASE_PAGE_NUMBER_SUCCESS = 'increase_page_number_success';
