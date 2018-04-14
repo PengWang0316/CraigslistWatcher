0b36ddcd49e294700be5d339753fb6ee4c679cec
Kevin W <rocwang@cityuniversity.edu>
2018-04-14 12:08:10 -0700

    Added three new reducers to index reducer.

--- App/reducers/Index.js
+++ App/reducers/Index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
-// import { routerReducer } from 'react-router-redux';
-
 
 import lists from './Lists';
+import city from './City';
+import category from './Category';
+import keyword from './Keyword';
 
 export default combineReducers({
+  category,
+  city,
+  keyword,
   lists
 });
