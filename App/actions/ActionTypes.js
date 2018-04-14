4fd93490e44fc9610dcc90bd55bb56f9e4266651
Kevin W <rocwang@cityuniversity.edu>
2018-04-14 12:08:38 -0700

    Added new ActionTypes for three new reducers.

--- App/actions/ActionTypes.js
+++ App/actions/ActionTypes.js
@@ -1,2 +1,5 @@
 export const FETCH_LISTS_SUCCESS = 'fetch_lists_success';
 export const FETCH_MORE_LISTS_SUCCESS = 'fetch_more_lists_success';
+export const CHANGE_CITY_SUCCESS = 'change_city_success';
+export const CHANGE_CATEGORY_SUCCESS = 'change_category_success';
+export const CHANGE_KEYWORD_SUCCESS = 'change_keword_success';
