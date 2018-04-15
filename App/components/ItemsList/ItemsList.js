c538003487e273a7ce36cca1930a2f9d9be93e3f
Kevin W <rocwang@cityuniversity.edu>
2018-04-14 17:19:39 -0700

    Using new api in the ItemsList component.

--- App/components/ItemsList/ItemsList.js
+++ App/components/ItemsList/ItemsList.js
@@ -1,20 +1,93 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import { View, ActivityIndicator, FlatList } from 'react-native';
+// import { ListItem, Icon } from 'react-native-elements';
 
-import { fetchLists } from '../../actions/ListActions';
+import Styles from './Styles';
+import { fetchLists, fetchMoreLists } from '../../actions/ListActions';
+import { increasePageNumber } from '../../actions/CraigslistStatesActions';
+import CraigslistListItem from '../CraigslistListItem';
 
 /** The component are used to show the item list that fetched from CraigsList. */
 class ItemsList extends Component {
+  static propTypes = {
+    lists: PropTypes.array.isRequired,
+    craigslistStates: PropTypes.object.isRequired,
+    fetchLists: PropTypes.func.isRequired,
+    fetchMoreLists: PropTypes.func.isRequired,
+    increasePageNumber: PropTypes.func.isRequired
+  };
+
+  /**
+   * If a new Lists data is loaded, set refreshing state to false.
+   * @param {object} nextProps contains new prop values.
+   * @param {object} prevState contains old state values.
+   * @return {object} Return a new state.
+   */
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.lists !== nextProps.lists) return { refreshing: false };
+    return null;
+  }
+
+  state = { refreshing: false };
+
   /**
    * Fetching the inintal list data from CraigsList.
-   * @param {object} props contains the props values.
    * @return {null} No return.
    */
-  constructor(props) {
-    super(props);
-    props.fetchLists(0);
+  componentDidMount() {
+    this.props.fetchLists({
+      city: this.props.craigslistStates.city,
+      keyword: this.props.craigslistStates.keyword,
+      category: this.props.craigslistStates.category
+    });
   }
+
+  /**
+   * Loading new data from the data source.
+   * @return {null} No return.
+   */
+  handleRefresh = () => this.setState({ refreshing: true }, () => this.props.fetchLists({
+    city: this.props.craigslistStates.city,
+    keyword: this.props.craigslistStates.keyword,
+    category: this.props.craigslistStates.category
+  }));
+
+  handleEndReached = () => {
+    this.props.fetchMoreLists({
+      page: this.props.craigslistStates.pageNumber + 1,
+      city: this.props.craigslistStates.city,
+      keyword: this.props.craigslistStates.keyword,
+      category: this.props.craigslistStates.category
+    });
+    this.props.increasePageNumber();
+  };
+
+  /**
+   * Rendering eatch item for the FlatList.
+   * @return {jsx} Return a CraigslistListItem component.
+   */
+  renderItem = ({ item }) => <CraigslistListItem item={item} />;
+
+  /**
+   * Rendering the footer for the FlatList.
+   * @return {jsx} Return null when lists is empty. Return an activity indicator when the lists is not empty.
+   */
+  renderFooter = () => {
+    if (this.props.lists.length === 0) return null;
+    return (
+      <View style={Styles.indicatorViewStyle}>
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  /**
+   * Rendering the separator for the FlatList.
+   * @return {jsx} Return a view with separator style.
+   */
+  renderSeparator = () => <View style={Styles.separatorStyle} />;
 
   /**
    * Render the jsx for the component.
@@ -26,15 +99,30 @@ class ItemsList extends Component {
     return (
       <View>
         {lists.length === 0 && <ActivityIndicator size="large" color="gray" />}
-        {lists.length !== 0 && <Text>{lists[0].title}</Text>}
+        {lists.length !== 0 &&
+        <FlatList
+          data={lists}
+          keyExtractor={item => item.dataId}
+          renderItem={this.renderItem}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListFooterComponent={this.renderFooter}
+          refreshing={this.state.refreshing}
+          onRefresh={this.handleRefresh}
+          onEndReached={this.handleEndReached}
+          onEndThreshold={10}
+        />
+        }
       </View>
     );
   }
 }
 const mapStateToProps = state => ({
-  lists: state.lists
+  lists: state.lists,
+  craigslistStates: state.craigslistStates
 });
 const mapDispatchToProps = dispatch => ({
-  fetchLists: page => dispatch(fetchLists(page))
+  fetchLists: parameter => dispatch(fetchLists(parameter)),
+  fetchMoreLists: parameter => dispatch(fetchMoreLists(parameter)),
+  increasePageNumber: () => dispatch(increasePageNumber())
 });
 export default connect(mapStateToProps, mapDispatchToProps)(ItemsList);
