4c40608133a48f5fc57676f590ca1a6d1f7391b7
Kevin W <rocwang@cityuniversity.edu>
2018-04-15 16:59:52 -0700

    Added test code and snapshot file for the ItemsList component.

--- __tests__/components/ItemsList.js
+++ __tests__/components/ItemsList.js
@@ -9,7 +9,9 @@ jest.mock('../../App/components/CraigslistListItem', () => 'CraigslistListItem')
 describe('Test ItemsList', () => {
   const defaultPorps = {
     lists: [],
-    craigslistStates: {},
+    craigslistStates: {
+      city: 'city', keyword: 'keyword', category: 'category', pageNumber: 2
+    },
     fetchLists: jest.fn(),
     fetchMoreLists: jest.fn(),
     increasePageNumber: jest.fn()
@@ -17,8 +19,62 @@ describe('Test ItemsList', () => {
 
   const getComponent = (props = defaultPorps) => shallow(<ItemsList {...props} />);
 
-  test('initial states', () => {
+  test('initial states and componentDidMount', () => {
     const component = getComponent();
     expect(component.state('refreshing')).toBe(false);
+    expect(defaultPorps.fetchLists).toHaveBeenCalledWith({
+      city: 'city', keyword: 'keyword', category: 'category'
+    });
+    expect(defaultPorps.fetchLists).toHaveBeenCalledTimes(1);
   });
+
+  test('getDerivedStateFromProps', () => {
+    const component = getComponent();
+    const newLists = [];
+    component.setState({ refreshing: true });
+    component.setProps({ lists: newLists });
+    expect(component.state('refreshing')).toBe(false);
+    component.setState({ refreshing: true, lists: newLists });
+    component.setProps({ lists: newLists });
+    expect(component.state('refreshing')).toBe(true);
+  });
+
+  test('handleRefresh', () => {
+    const component = getComponent({ ...defaultPorps, craigslistStates: { city: 'new city', keyword: 'new keyword', category: 'new category' } });
+    component.instance().handleRefresh();
+    expect(component.state('refreshing')).toBe(true);
+    expect(defaultPorps.fetchLists).toHaveBeenCalledWith({
+      city: 'new city', keyword: 'new keyword', category: 'new category'
+    });
+  });
+
+  test('handleEndReached', () => {
+    const component = getComponent();
+    component.instance().handleEndReached();
+    expect(defaultPorps.fetchMoreLists).toHaveBeenCalledWith({
+      city: 'city', keyword: 'keyword', category: 'category', page: 3
+    });
+    expect(defaultPorps.increasePageNumber).toHaveBeenCalledTimes(1);
+  });
+
+  test('renderItem snapshot', () => {
+    const component = getComponent();
+    expect(renderer.create(component.instance().renderItem({ item: 'item' })).toJSON()).toMatchSnapshot();
+  });
+
+  test('renderSeparator snapshot', () => {
+    const component = getComponent();
+    expect(renderer.create(component.instance().renderSeparator()).toJSON()).toMatchSnapshot();
+  });
+
+  test('renderFooter snapshot', () => {
+    const component = getComponent();
+    expect(component.instance().renderFooter()).toBeNull();
+    component.setProps({ lists: [1] });
+    expect(renderer.create(component.instance().renderFooter()).toJSON()).toMatchSnapshot();
+  });
+
+  test('snapshot test empty lists', () => expect(renderer.create(<ItemsList {...defaultPorps} />).toJSON()).toMatchSnapshot());
+
+  test('snapshot test not empty lists', () => expect(renderer.create(<ItemsList {...{ ...defaultPorps, lists: [1] }} />).toJSON()).toMatchSnapshot());
 });
