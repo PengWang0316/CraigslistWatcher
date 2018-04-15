26fbcd49a9dc530270511b181afb330e2fb50989
Kevin W <rocwang@cityuniversity.edu>
2018-04-14 17:19:03 -0700

    Fixed a variable name issue.

--- App/reducers/CraigslistStates.js
+++ App/reducers/CraigslistStates.js
@@ -1,6 +1,6 @@
 import { CHANGE_CITY_SUCCESS, CHANGE_CATEGORY_SUCCESS, CHANGE_KEYWORD_SUCCESS, INCREASE_PAGE_NUMBER_SUCCESS } from '../actions/ActionTypes';
 
-const craigslistStates = (state = { city: 'www', category: 'sss', page: 1 }, action) => {
+const craigslistStates = (state = { city: 'www', category: 'sss', pageNumber: 1 }, action) => {
   switch (action.type) {
     case CHANGE_CITY_SUCCESS:
       return { ...state, city: action.city };
@@ -9,7 +9,7 @@ const craigslistStates = (state = { city: 'www', category: 'sss', page: 1 }, act
     case CHANGE_KEYWORD_SUCCESS:
       return { ...state, city: action.keyword };
     case INCREASE_PAGE_NUMBER_SUCCESS:
-      return { ...state, pageNumber: action.pageNumber + 1 };
+      return { ...state, pageNumber: state.pageNumber + 1 };
     default:
       return state;
   }
