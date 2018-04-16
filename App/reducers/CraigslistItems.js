bd4cde4d2ba3f3e3c5caae162379938b24c98962
Kevin W <rocwang@cityuniversity.edu>
2018-04-16 15:51:13 -0700

    Removed CraigslistItems.js

--- App/reducers/CraigslistItems.js
+++
@@ -1,7 +0,0 @@
-const craigslistItems = (state = {}, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
-export default craigslistItems;
--- App/reducers/Index.js
+++ App/reducers/Index.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux';
 
 import lists from './Lists';
 import craigslistStates from './CraigslistStates';
-import craigslistItems from './CraigslistItems';
 
 export default combineReducers({
-  craigslistItems,
   craigslistStates,
   lists
 });
