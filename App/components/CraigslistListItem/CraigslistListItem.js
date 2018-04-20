3999841deb168ad09043a81ebc0017048d740769
Kevin W <rocwang@cityuniversity.edu>
2018-04-19 22:51:47 -0700

    Added clicking left icon function to replace an icon to an avatar.

--- App/components/CraigslistListItem/CraigslistListItem.js
+++ App/components/CraigslistListItem/CraigslistListItem.js
@@ -1,16 +1,52 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { ListItem, Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 import styles from './Styles';
+import { fetchCraigslistItem } from '../../actions/CraigslistStatesActions';
 
 /** ListItem for CraigsList's FlatList */
-class CraigslistListItem extends PureComponent {
+export class CraigslistListItem extends PureComponent {
   static propTypes = {
     item: PropTypes.object.isRequired
   };
 
-  handleLeftIconClick = () => console.log('ok');
+  /**
+   * Update avatar and leftIcon state after a new item was added to Redux's craigslistStates.items.
+   * @param {object} nextProps is the changed props values.
+   * @param {object} prevState is the old state values.
+   * @return {object} Return a object that contains state that need to be updated.
+   */
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.leftIcon !== null && nextProps.craigslistStates.items[nextProps.item.dataId])
+      return {
+        leftIcon: null,
+        avatar: { uri: nextProps.craigslistStates.items[nextProps.item.dataId].images[0] }
+      };
+    return null;
+  }
+
+  /**
+   * Initialing states for the component.
+   * @param {object} props contains props values.
+   * @return {null} No return.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatar: this.props.craigslistStates.items[this.props.item.dataId] ?
+        { uri: this.props.craigslistStates.items[this.props.item.dataId].images[0] } : null,
+      leftIcon: this.props.craigslistStates.items[this.props.item.dataId] ? null : <Icon name="wallpaper" raised onPress={this.handleLeftIconClick} />
+    };
+  }
+
+  handleLeftIconClick = () => {
+    if (this.props.craigslistStates.items[this.props.item.dataId]) this.setState({
+      avatar: { uri: this.props.craigslistStates.items[this.props.item.dataId] },
+      leftIcon: null
+    }); else this.props.fetchCraigslistItem(this.props.item.url).catch(err => console.log(err));
+  };
 
   /**
    * Rendering the jsx for the component.
@@ -21,13 +57,23 @@ class CraigslistListItem extends PureComponent {
     const { item } = this.props;
     return (
       <ListItem
+        roundAvatar
+        avatar={this.state.avatar}
         title={item.title}
         subtitle={`${item.region} ${item.datetime}`}
-        leftIcon={<Icon name="wallpaper" raised onPress={this.handleLeftIconClick} />}
+        leftIcon={this.state.leftIcon}
         badge={{ value: item.price }}
         containerStyle={styles.containerStyle}
       />
     );
   }
 }
-export default CraigslistListItem;
+/* istanbul ignore next */
+const mapStateToProps = state => ({
+  craigslistStates: state.craigslistStates
+});
+/* istanbul ignore next */
+const mapDispatchToProps = dispatch => ({
+  fetchCraigslistItem: url => dispatch(fetchCraigslistItem(url))
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CraigslistListItem);
