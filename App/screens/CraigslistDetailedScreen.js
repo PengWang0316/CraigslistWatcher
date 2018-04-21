7899d43d18696cfc36aa2a3161b311b66e6f428d
Kevin W <rocwang@cityuniversity.edu>
2018-04-21 15:51:11 -0700

    Added the react-native-snap-carousel library to the project.

--- App/screens/CraigslistDetailedScreen.js
+++ App/screens/CraigslistDetailedScreen.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { SafeAreaView } from 'react-navigation';
 
 import DarkGreenStatusBar from '../components/DarkGreenStatusBar';
+import ImageCarousel from '../components/ImageCarousel';
 import Styles from './Styles';
 
-const CraigslistDetailedScreen = () => (
+const CraigslistDetailedScreen = props => (
   <SafeAreaView style={Styles.mainView}>
     <DarkGreenStatusBar />
-
+    <ImageCarousel dataId={props.navigation.state.dataId} />
   </SafeAreaView>
 );
 export default CraigslistDetailedScreen;
